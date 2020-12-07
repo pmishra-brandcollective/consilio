e6c1d48a9cf7797ba5dede9f9a50305287a1f6b2
brendino500 <brenda.ty@live.com>
2020-12-07 14:34:44 +0000

    controlllers user

---
+++ config/router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const auth = require('../controllers.auth')
+
+// Log Related
+router.route('/register')
+  .post(auth.register)
+router.route('/login')
+  .post(auth.login)
+
+// Profiles Related
+
