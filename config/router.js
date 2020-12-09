98da52e37125143fd1e95c2ac5bb304a7268cdfc
brendino500 <brenda.ty@live.com>
2020-12-09 11:51:57 +0000

    items schema seed

--- config/router.js
+++ config/router.js
@@ -1,5 +1,14 @@
 const router = require('express').Router()
 const auth = require('../controllers/auth')
+const items = require('../controllers/items')
+
+// Items Related
+
+router.route('/items')
+  .get(items.index)
+
+router.route('/items/:id')
+  .get(items.show)
 
 // Log Related
 router.route('/register')
--- models/item.js
+++ models/item.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 const itemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   item: { type: String, required: true },
   category: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: Array, required: true },
   price: { type: Number, required: true }
 })
 
