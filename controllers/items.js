f63e1c89c1e3f83a74bec940ee0c94bb900557dc
brendino500 <brenda.ty@live.com>
2020-12-09 10:55:10 +0000

    controllers/items.js

--- controllers/items.js
+++ controllers/items.js
@@ -1,4 +1,27 @@
 const Items =  require('../models/item')
+const { notFound } = require('../lib/errorMessage')
 
-const { notFound, unauthorized } = require('../lib/')
+async function itemsIndex(req, res, next) {
+  try {
+    const items = await Items.find()
+    if (!items) throw new Error(notFound)
+    res.status(200).json(items)
+  } catch (err) {
+    next(err)
+  }
+}
 
+async function itemShow(req, res, next) {
+  try {
+    const item = await Items.findById(req.params.id).populate('user')
+    if (!item) throw new Error(notFound)
+    res.status(200).json(item)
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  index: itemsIndex,
+  show: itemShow
+}
