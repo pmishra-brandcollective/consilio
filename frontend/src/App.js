f6fa3b7663b87b4fca7dd7f33cee44ec155a51ad
brendino500 <brenda.ty@live.com>
2021-03-04 15:20:18 +0000

    update to tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import Navbar from '../src/components/common/Navbar'
-
 function App() {
-  return <>
-  <Navbar />
-  </>;
+  return (
+    <>
+      <h1
+        className="sm:text-green-700 sm:text-9xl md:text-pink-100
+      text-9xl text-black-50 bg-blue-100 tracking-widest hover:text-blue-900 hover:bg-pink-100"
+      >
+        Consilio
+      </h1>
+    </>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.export = function(app) {
+module.exports = function (app) {
   app.use(createProxyMiddleware('/api', { target: 'http://localhost:7000' }))
 }
-
